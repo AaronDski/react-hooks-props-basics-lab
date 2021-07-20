fad616d08045ef6142b7c0424cf15a6a9dfe264e
Aaron Dereszynski <aarondski123@gmail.com>
2021-07-20 15:00:45 -0600

    commit tree bd3c99ec2ce77903f3fedc3b0eb9990feef0667f

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,21 @@
 import React from "react";
+import Links from './Links'
 
-function About() {
+function About(prop) {
+  const bioText = prop.bio
+
+  function bio(){
+    if (bioText) {
+    return <p>{prop.bio}</p>
+    }
+  }
+
   return (
     <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <h2>About Me</h2> 
+      {bio()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {<Links />}
     </div>
   );
 }
